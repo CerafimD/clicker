e4ee86ca2a24aebcef689111163b7ef84a705028
Danil Serafim <danilcerafim@gmail.com>
2024-05-03 20:46:48 +0500

    Апгрейд бустов и их базовое функционирование (сделано в понедельник, запоздалый коммит)

--- backend/urls.py
+++ backend/urls.py
@@ -4,7 +4,12 @@ from . import views
 
 boosts = views.BoostViewSet.as_view({'get': 'list',
                                       'post': 'create'})
+
+lonely_boost = views.BoostViewSet.as_view({'put':'partial_update'})
+
 urlpatterns = [
     path('call_click/', views.call_click),
     path('boosts/', boosts,name = 'boosts'),
+    #path('buyBoost/',views.partial_update,name = 'buyBoost'),
+    path('boost/<int:pk>/', lonely_boost,name = 'boost')
 ]
